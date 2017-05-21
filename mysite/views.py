1c6019f89df479871f37a65e629d45f5f7c77384
BenTaub <btaub.48104@gmail.com>
2017-05-21 18:21:08 -0400

    Start of django training

--- mysite/views.py
+++ mysite/views.py
@@ -1,3 +1,6 @@
+# from django.template.loader import get_template
+# from django.template import Context
+from django.shortcuts import render
 from django.http import Http404, HttpResponse
 import datetime
 
@@ -7,9 +10,15 @@ def hello(request):
 
 def current_datetime(request):
     now = datetime.datetime.now()
-    html = "<html><body>UTC time is now %s</body></html>" % now
-    html += "<p><html><body><strong>A2 time is now %s</strong></body></html></p>" % (now + datetime.timedelta(hours=-4))
-    return HttpResponse(html)
+    return render(request, 'current_datetime.html', {'current_date': now})
+
+    # t = get_template('current_datetime.html')
+    # html = t.render(Context({'current_date': now}))
+
+    # html = "<html><body>UTC time is now %s</body></html>" % now
+    # html += "<p><html><body><strong>A2 time is now %s</strong></body></html></p>" % (now + datetime.timedelta(hours=-4))
+
+    # return HttpResponse(html)
 
 
 def hours_ahead(request, offset):
@@ -19,6 +28,8 @@ def hours_ahead(request, offset):
         raise Http404()
     now = datetime.datetime.now()
     dt = now + datetime.timedelta(hours=offset)
-    html = "<html><body>The UTC time is currently:%s</body></html>" %(now)
-    html += "<p><body>In %s hour(s) the UTC time will be: %s</body></p>" %(offset, dt)
-    return HttpResponse(html)
+    return render(request=request, template_name='hours_ahead.html',
+                  context={'hour_offset': offset, 'next_time': dt, 'now': now})
+    # html = "<html><body>The UTC time is currently:%s</body></html>" %(now)
+    # html += "<p><body>In %s hour(s) the UTC time will be: %s</body></p>" %(offset, dt)
+    # return HttpResponse(html)
